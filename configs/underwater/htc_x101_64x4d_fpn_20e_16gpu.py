14960ff72a0c4bc4bde33e347b16f3d92fa24743
MKFMIKU <mikumkf@gmail.com>
2020-03-26 15:53:44 +0800

    update scale

--- configs/underwater/htc_x101_64x4d_fpn_20e_16gpu.py
+++ configs/underwater/htc_x101_64x4d_fpn_20e_16gpu.py
@@ -166,8 +166,7 @@ img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375],
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=[(4096, 600), (4096, 1000)],
-         multiscale_mode='range', keep_ratio=True),
+    dict(type='Resize', img_scale=[(1920, 1080), (2880, 1620), (3840, 2160)], multiscale_mode='range', keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -178,7 +177,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(4096, 1000),
+        img_scale=(2880, 1620),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
